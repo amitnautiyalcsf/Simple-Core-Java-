8ad6b853cb5dc613e82c9bdcc65923f6160e9df9
Amit Nautiyal <amitnautiyal.cs@gmail.com>
2019-01-25 17:21:53 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,25 @@
+public class Main{
+
+     public static int add(int a, int b) {
+         return a+b;
+     }
+     public static int sub(int c, int d) {
+         
+         return c-d; 
+     }
+     public static int mult(int f, int g) {
+         
+         return f*g;
+     }
+     public static int div(int h, int i) {
+         
+         return h/i;
+     }
+     
+     public static void main(String []args){
+        System.out.println(add(3,2));
+        System.out.println(sub(3,2));
+        System.out.println(mult(3,2));
+        System.out.println(div(4,2));
+     }
+}
