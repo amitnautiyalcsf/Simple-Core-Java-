c828c395d26ab37b1e695d831cd0979bfff13423
Amit Nautiyal <amitnautiyal.cs@gmail.com>
2019-01-28 20:25:31 +0530

    Create table.java

---
+++ table.java
@@ -0,0 +1,25 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    
+		int table[][] = new int[4][3];
+		
+		table[0][0]=10;
+		table[0][1]=10;
+		table[0][2]=10;
+		
+		table[1][0]=10;
+		table[1][1]=10;
+		table[1][2]=10;
+		
+		table[2][0]=10;
+		table[2][1]=10;
+		table[2][2]=10;
+		
+		table[3][0]=10;
+		table[3][1]=10;
+		table[3][2]=10;
+		
+		System.out.println(table[0][2].length);
+	}
+}
